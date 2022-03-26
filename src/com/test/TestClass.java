ca9e9df588f4c713bdf0abf2bfac25b54d8b3192
Neetish Bhardwaj <neetishb@gmail.com>
2022-03-26 15:27:16 +0530

    First Commit

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class TestClass {
+
+	public void test1()
+	{
+		System.out.println("This is Test1");
+	}
+	
+	public void test2()
+	{
+		System.out.println("This is Test2");
+	}
+}
